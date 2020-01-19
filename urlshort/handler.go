4f815eb0f094e50befe4998f8bad30aa9d59893c
Nando Theessen <nando.theessen@gmail.com>
2020-01-19 11:46:41 +0100

    Add implementation for MapHandler

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -11,18 +11,15 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	
-	// val, ok := pathsToUrls[]
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+	return func (rw http.ResponseWriter, req *http.Request) {
 		
-		url := pathsToUrls[r.URL.String()]
-		if url != "" {
-			http.Redirect(w, r, url, http.StatusSeeOther)
+		path := req.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(rw, req, dest, http.StatusFound)
 			return
 		}
-		fallback.ServeHTTP(w, r)
-	})
+		fallback.ServeHTTP(rw, req)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -41,7 +38,7 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
-}
+// func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+// 	// TODO: Implement this...
+// 	return nil, nil
+// }
