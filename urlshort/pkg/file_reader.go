0547a2cc140a236a3ba485ffba40296646dcb6ed
Nando Theessen <nando.theessen@gmail.com>
2020-01-23 23:04:58 +0100

    Adds internal module for file handling

---
+++ urlshort/pkg/file_reader.go
@@ -0,0 +1,17 @@
+package reader 
+
+import (
+	"io/ioutil"
+	"fmt"
+)
+
+// ProcessFile takes a filename as argument and returns a bytearray with the 
+// contents of that file or an error
+func ProcessFile(filename string) (contents []byte, err error) {
+	contents, err = ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return contents, nil
+}
